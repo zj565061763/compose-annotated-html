24c0c5bbeda66fba44a4b0db9d3783344680b13a
zjun <565061763@qq.com>
2024-11-25 17:26:51 +0800

    inlineTextContentFlow

--- lib/src/main/java/com/sd/lib/compose/annotated/html/tags/TagBuilder.kt
+++ lib/src/main/java/com/sd/lib/compose/annotated/html/tags/TagBuilder.kt
@@ -1,9 +1,31 @@
 package com.sd.lib.compose.annotated.html.tags
 
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.Placeholder
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import org.jsoup.nodes.Element
 
 abstract class TagBuilder {
+   private val _inlineTextContentFlow = MutableStateFlow<Map<String, InlineTextContent>>(emptyMap())
+
+   val inlineTextContentFlow: Flow<Map<String, InlineTextContent>>
+      get() = _inlineTextContentFlow.asStateFlow()
+
+   protected fun addInlineTextContent(
+      id: String,
+      placeholder: Placeholder,
+      children: @Composable (String) -> Unit,
+   ) {
+      _inlineTextContentFlow.update {
+         it + (id to InlineTextContent(placeholder, children))
+      }
+   }
+
    open fun buildText(
       builder: AnnotatedString.Builder,
       text: String,
