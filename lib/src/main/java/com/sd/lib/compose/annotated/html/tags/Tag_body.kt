f09e2144ef7a8444d99f8a6baf223d4d67a5df9c
zjun <565061763@qq.com>
2024-11-26 21:24:00 +0800

    line

--- lib/src/main/java/com/sd/lib/compose/annotated/html/tags/Tag_body.kt
+++ lib/src/main/java/com/sd/lib/compose/annotated/html/tags/Tag_body.kt
@@ -1,5 +1,12 @@
 package com.sd.lib.compose.annotated.html.tags
 
+import androidx.compose.ui.text.AnnotatedString
 import com.sd.lib.compose.annotated.html.AnnotatedHtml
+import org.jsoup.nodes.Element
+import org.jsoup.nodes.TextNode
 
-class Tag_body : AnnotatedHtml.Tag()
+class Tag_body : AnnotatedHtml.Tag() {
+   override fun elementText(builder: AnnotatedString.Builder, element: Element, textNode: TextNode) {
+      builder.append(textNode.text().trim())
+   }
+}
