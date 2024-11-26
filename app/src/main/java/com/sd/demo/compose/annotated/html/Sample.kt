023bdac6e08ca05307a313cfdac49bc325943e40
zjun <565061763@qq.com>
2024-11-26 20:15:07 +0800

    Simplify

--- app/src/main/java/com/sd/demo/compose/annotated/html/Sample.kt
+++ app/src/main/java/com/sd/demo/compose/annotated/html/Sample.kt
@@ -42,39 +42,5 @@ private fun Content(
    }
 }
 
-val htmlContent = """
-<body>
-    <h1>h1</h1>
-    <h2>h2</h2>
-    <h3>h3</h3>
-    <h4>h4</h4>
-    <h5>h5</h5>
-    <h6>h6</h6>
-    
-    <p style="text-align: start;">start</p>
-    <p style="text-align: center;">center
-      <p style="text-align: start;">center start</p>
-    </p>
-    <p style="text-align: end;">end</p>
-    
-    <p style="color: green;">color</p>
-    <p style="background-color: red;">background-color</p>
-
-    <p style="text-decoration: line-through;">decoration line through</p>
-    <p style="text-decoration: underline;">decoration underline</p>
-    
-    <p>b:<b>bbb</b> i:<i>iii</i> u:<u>uuu</u> strong:<strong>strong</strong> em:<em>em</em></p>
-    
-    <p>test a <a href="https://www.baidu.com">click</a> here</p>
-    
-    <p>image:<img src="" alt="" /></p>
-    
-    <ul><li>ul li 1</li><li>ul li 2</li></ul>
-    <ol><li>ol li 1</li><li>ol li 2</li></ol>
-    
-    <blockquote>This is a blockquote.</blockquote>
-    
-    <p>Here is a line break:</p>
-    before br<br/>after br
-</body>
-"""
+val htmlContent =
+   """<body><h1>h1</h1><h2>h2</h2><h3>h3</h3><h4>h4</h4><h5>h5</h5><h6>h6</h6><p style="text-align: start;">start</p><p style="text-align: center;">center<p style="text-align: start;">center start</p></p><p style="text-align: end;">end</p><p style="color: green;">color</p><p style="background-color: red;">background-color</p><p style="text-decoration: line-through;">decoration line through</p><p style="text-decoration: underline;">decoration underline</p><p>b:<b>bbb</b>i:<i>iii</i>u:<u>uuu</u>strong:<strong>strong</strong>em:<em>em</em></p><p>test a<a href="https://www.baidu.com">click</a>here</p><p>image:<img src=""alt=""/></p><ul><li>ul li 1</li><li>ul li 2</li></ul><ol><li>ol li 1</li><li>ol li 2</li></ol><blockquote>This is a blockquote.</blockquote><p>Here is a line break:</p>before br<br/>after br</body>"""
--- lib/src/main/java/com/sd/lib/compose/annotated/html/tags/Tag_body.kt
+++ lib/src/main/java/com/sd/lib/compose/annotated/html/tags/Tag_body.kt
@@ -1,12 +1,5 @@
 package com.sd.lib.compose.annotated.html.tags
 
-import androidx.compose.ui.text.AnnotatedString
 import com.sd.lib.compose.annotated.html.AnnotatedHtml
-import org.jsoup.nodes.Element
-import org.jsoup.nodes.TextNode
 
-class Tag_body : AnnotatedHtml.Tag() {
-   override fun elementText(builder: AnnotatedString.Builder, element: Element, textNode: TextNode) {
-      builder.append(textNode.text().trim())
-   }
-}
+class Tag_body : AnnotatedHtml.Tag()
